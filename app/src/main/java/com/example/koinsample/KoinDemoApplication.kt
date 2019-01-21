2ae0893a682bfb172fe77ced44298ba38183ff2a
su_myat <developersumyat@gmail.com>
2019-01-21 10:55:57 +0630

    built modules and context application of koin

---
+++ app/src/main/java/com/example/koinsample/KoinDemoApplication.kt
@@ -0,0 +1,12 @@
+package com.example.koinsample
+
+import android.app.Application
+import org.koin.android.ext.android.startKoin
+
+class KoinDemoApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(this, listOf(applicationModule))
+    }
+}
